dc816cfe400701964511f14009984dd71e10e680
dorothy <dorothy.luluya@gmail.com>
2025-08-22 18:45:40 +0800

    toRefs和toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,50 +1,48 @@
 <template>
     <div class="person">
-        <h2>汽车信息：一辆{{ car.beand }}车,价值{{ car.price }}万</h2>
-        <button @click="changePricep">修改汽车的价格</button>
-        <button @click="changeBrand">修改汽车的品牌</button>
-        <button @click="changeCar">修改汽车</button>
-        <hr>
-        <h2>当前求和为:{{ sum }}</h2>
-        <button @click="changeSum">点我sum加1</button>
+        <h2>姓名：{{ Person.name }}</h2>
+        <h2>年龄：{{ Person.age }},{{ age }}</h2>
+        <button @click="changeName">修改姓名</button>
+        <button @click="changeAge">修改年龄</button>
     </div>
 </template>
 
 
 <script lang="ts" setup name="Person">
-import { ref, reactive } from 'vue'
+import { reactive, toRefs,toRef } from 'vue'
 
 
 // 数据
-let car = reactive({ beand: '奔驰', price: 100 })
-let sum = ref(0)
+let Person = reactive({
+    name: '张三',
+    age: 18
+})
+
+let { name,age} = toRefs(Person)
+let nl = toRef(Person,'age')
+console.log(nl.value)
+// 方法
+function changeName() {
+    name.value += '~'
+   console.log(name,Person.name)
+}
+function changeAge() {
+    age.value += 1
+}
+</script>
+
+
+
+
+
+
+
 
 
 
 
 // 方法
-function changeBrand() {
-    car.beand = '宝马'
-}
-function changePricep() {
-    car.price += 10
-    console.log(car.price)
-}
-
-
-function changeCar() {
-    // car.beand = '奥迪'
-    // car.price = 120
-    car = { beand: '奥迪', price: 1200 }
-
-}
-
-function changeSum() {
-    sum.value += 1
-}
-
-
-
+<script>
 </script>
 
 
