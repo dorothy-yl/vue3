09d995985c5e20845d0b1d787c767949157e1c4c
dorothy <dorothy.luluya@gmail.com>
2025-08-22 16:11:31 +0800

    setup语法糖

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    VueDevTools({
+      launchEditor: 'cursor', // Specify 'cursor' as the desired editor
+    }),
   ],
   resolve: {
     alias: {
