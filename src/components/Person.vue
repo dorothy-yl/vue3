26976bf3746315658db470b0a6ddb67e6edb7866
dorothy <dorothy.luluya@gmail.com>
2025-08-23 09:50:00 +0800

    computed的计算属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,51 +1,51 @@
 <template>
     <div class="person">
-        <h2>姓名：{{ Person.name }}</h2>
-        <h2>年龄：{{ Person.age }},{{ age }}</h2>
-        <button @click="changeName">修改姓名</button>
-        <button @click="changeAge">修改年龄</button>
+        姓：<input type="text" v-model="firstName"><br>
+        名：<input type="text" v-model="lastName"><br>
+        <button @click="changeFullName">将全名改为li-si</button>
+        全名：<span>{{ fullName }}</span><br>
+        全名：<span>{{ fullName }}</span><br>
+        全名：<span>{{ fullName }}</span><br>
+        全名：<span>{{ fullName }}</span><br>
+        全名：<span>{{ fullName }}</span><br>
+        全名：<span>{{ fullName }}</span><br>
     </div>
 </template>
 
 
-<script lang="ts" setup name="Person">
-import { reactive, toRefs,toRef } from 'vue'
 
+<script lang="ts" setup name="Person">
+import { ref, computed } from 'vue'
 
 // 数据
-let Person = reactive({
-    name: '张三',
-    age: 18
+let firstName = ref('zhang')
+let lastName = ref('san')
+
+
+
+// 方法,这么定义的firstName是一个计算属性，且是只读的
+
+// let fullName = computed(() =>{
+//     console.log(1)
+//     return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+// })
+
+let fullName = computed({
+    get() {
+        return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+    },
+    set(val) {
+        const [str1, str2] = val.split('-')
+        firstName.value = str1  
+        lastName.value = str2
+    }
 })
 
-let { name,age} = toRefs(Person)
-let nl = toRef(Person,'age')
-console.log(nl.value)
-// 方法
-function changeName() {
-    name.value += '~'
-   console.log(name,Person.name)
-}
-function changeAge() {
-    age.value += 1
+function changeFullName() {
+    fullName.value = 'li-si'
+
 }
 </script>
-
-
-
-
-
-
-
-
-
-
-
-// 方法
-<script>
-</script>
-
-
 
 
 <style scoped>
