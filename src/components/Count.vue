07684a9c42ff2659b006d7ca8e1d94a5798b8590
dorothy <dorothy.luluya@gmail.com>
2025-08-26 00:10:44 +0800

    存储+读取数据

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="count">
-      <h2>当前求和为：{{ sum }}</h2>
+      <h2>当前求和为：{{ countStore.sum }}</h2>
       <select v-model.number="n">
         <option value="1">1</option>
         <option value="2">2</option>
@@ -12,17 +12,21 @@
   </template>
   
   <script setup lang="ts" name="Count">
-    import { ref } from "vue";
+    import { ref,reactive } from "vue";
+    import { useCountStore } from "@/store/count";
+    const countStore = useCountStore()
+
+    // 以下两种方式都可以拿到state中的数据
+    // console.log('@@@',countStore.count)
+    // console.log('@@@',countStore.$state.count)
+    
     // 数据
-    let sum = ref(1) // 当前求和
-    let n = ref(1) // 用户选择的数字
+    let n = ref(0) // 用户选择的数字
   
     // 方法
     function add(){
-      sum.value += n.value
     }
     function minus(){
-      sum.value -= n.value
     }
   </script>
   
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import {createApp} from 'vue'
 // 引入App根组件
 import App from './components/App.vue'
 
+
+// 引入pinia
+import {createPinia} from 'pinia'
+
+
 // 创建一个应用
 const app = createApp(App)
+
+// 创建pinia实例
+const pinia = createPinia()
+// 使用pinia
+app.use(pinia)
 // 挂载整个应用到app容器中
 app.mount('#app')
