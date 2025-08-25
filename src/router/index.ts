1ae971f6fe473020d44a1480245e255a5e185d04
dorothy <dorothy.luluya@gmail.com>
2025-08-25 16:19:55 +0800

    路由_params参数

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,34 @@
 
 
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory,createWebHashHistory,} from 'vue-router'
 import Home from '@/pages/Home.vue'
 import About from '@/pages/About.vue'
 import News from '@/pages/News.vue'
+import Detail from '@/pages/Detail.vue'
 
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
         {
+            name:'zhuye',
             path:'/home',
             component:Home
         },
         {
+            name:'guanyu',
             path:'/about',
             component:About
         },
         {
+            name:'xinwen',
             path:'/news',
-            component:News
+            component:News,
+            children:[
+                {
+                    path:'detail/:id/:title/:content',
+                    component:Detail
+                }
+            ]
         }
     ]
 })
