5dd2d310f955c51c0162eb56464e9bb77089d939
dorothy <dorothy.luluya@gmail.com>
2025-08-26 01:31:52 +0800

    storeToRefe和getters的使用

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,56 +1,48 @@
 <template>
-    <div class="count">
-      <h2>当前求和为：{{ countStore.sum }}</h2>
-      <h3>欢迎来到:{{ countStore.school }}，坐落于：{{ countStore.address }}</h3>
-      <select v-model.number="n">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-      </select>
-      <button @click="add">加</button>
-      <button @click="minus">减</button>
-    </div>
-  </template>
-  
-  <script setup lang="ts" name="Count">
-    import { ref,reactive } from "vue";
-    import { useCountStore } from "@/store/count";
-    const countStore = useCountStore()
-    
-    // 数据
-    let n = ref(0) // 用户选择的数字
-  
-    // 方法
-    function add(){
-       // 第一种修改方式
-    // countStore.sum += 1
+  <div class="count">
+    <h2>当前求和为：{{ sum }}</h2>
+    <h3>欢迎来到:{{ school }}，坐落于：{{ address }}</h3>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="add">加</button>
+    <button @click="minus">减</button>
+  </div>
+</template>
 
-    // // 第二种修改方式
-    //  countStore.$patch({
-    //   sum:6,
-    //   school:'尚硅谷',
-    //   minus:1,
-    //   address:'北京'
-    // }) 
+<script setup lang="ts" name="Count">
+  import { ref,reactive,toRefs } from "vue";
+  import {storeToRefs} from 'pinia'
+  // 引入useCountStore
+  import {useCountStore} from '@/store/count'
+  // 使用useCountStore，得到一个专门保存count相关的store
+  const countStore = useCountStore()
+  // storeToRefs只会关注sotre中数据，不会对方法进行ref包裹
+  const {sum,school,address} = storeToRefs(countStore)
+  // console.log('!!!!!',storeToRefs(countStore))
 
-    // 第三种修改方式
+  // 数据
+  let n = ref(1) // 用户选择的数字
+  // 方法
+  function add(){
     countStore.increment(n.value)
+  }
+  function minus(){
+    countStore.sum -= n.value
+  }
+</script>
 
-}
-function minus(){
-  
-}
-  </script>
-  
-  <style scoped>
-    .count {
-      background-color: skyblue;
-      padding: 10px;
-      border-radius: 10px;
-      box-shadow: 0 0 10px;
-    }
-    select,button {
-      margin: 0 5px;
-      height: 25px;
-    }
-  </style>
+<style scoped>
+  .count {
+    background-color: skyblue;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px;
+  }
+  select,button {
+    margin: 0 5px;
+    height: 25px;
+  }
+</style>
--- src/store/count.ts
+++ src/store/count.ts
@@ -1,25 +1,30 @@
-import { defineStore } from "pinia"
+import {defineStore} from 'pinia'
 
-export const useCountStore = defineStore('count', {
-
-    actions: {
-        increment(value:number) {
-            console.log('increment被调用了',value)
-            // console.log(this.sum)
-            if(this.sum <10){
-                this.sum += value
-            }
-            
-        }
-    },
-
-    // 真正存储数据的地方
-    state() {
-        return {
-            sum: 5,
-            school: 'atguigu',
-            address: '宏福科技园'
-        }
-
+export const useCountStore = defineStore('count',{
+  // actions里面放置的是一个一个的方法，用于响应组件中的“动作”
+  actions:{
+    increment(value:number){
+      console.log('increment被调用了',value)
+      if( this.sum < 10){
+        // 修改数据（this是当前的store）
+        this.sum += value
+      }
     }
+  },
+  // 真正存储数据的地方
+  state(){
+    return {
+      sum:3,
+      school:'atguigu',
+      address:'宏福科技园'
+    }
+  },
+  getters:{
+    bigSum(state){
+      return state.sum * 10
+    },
+    upperSchool(state){
+      return state.school.toUpperCase()
+    }
+  } 
 })
