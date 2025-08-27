4e21575f5d44a77f073b0482a817491a8f1c9b7d
dorothy <dorothy.luluya@gmail.com>
2025-08-27 16:30:29 +0800

    shallowRef和shallowReactive的应用

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import router from './router'
 
 // 创建应用
 const app = createApp(App)
-
-// 使用路由
-app.use(router)
-
-app.config.globalProperties.x = 99
-
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    x:number
-  }
-}
-
-app.directive('beauty',(element,{value})=>{
-  element.innerText += value
-  element.style.color = 'green'
-  element.style.backgroundColor = 'yellow'
-})
 
 // 挂载应用
 app.mount('#app')
