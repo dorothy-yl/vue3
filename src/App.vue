5ce8b83f6a68ce8584ed33c5faad3c91397d7970
dorothy <dorothy.luluya@gmail.com>
2025-08-27 18:38:01 +0800

    Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
-<template>  
-    <div class="outer" >
-        <h2>我是App组件</h2>
-        <img src="https://www.atguigu.com/images/index_new/logo.png" alt="">
-    <br>
-    <Modal/>
-    </div>
+<template>
+  <div class="app">
+    <h2>我是App组件</h2>
+    <Suspense>
+      <template v-slot:default>
+        <Child/>
+      </template>
+      <!-- <template v-slot:fallback> -->
+        <!-- <h2>加载中......</h2> -->
+      <!-- </template> -->
+    </Suspense>
+  </div>
 </template>
 
 <script setup lang="ts" name="App">
-import Modal from './Modal.vue'
+  import {Suspense} from 'vue'
+  // import Child from './Child'
 </script>
 
-
 <style>
-.outer {
+  .app {
     background-color: #ddd;
     border-radius: 10px;
+    padding: 10px;
     box-shadow: 0 0 10px;
-    padding: 5px;
-    width: 400px;
-    height:400px;
-filter:saturate(200%)
-}
-img {
-    width: 270px;
-   
-}
+  }
 </style>
